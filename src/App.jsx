ced9751794026e7d4d474d806f8c38bf928c45f7
Do Tien Dat <datblu2003@gmail.com>
2022-05-05 00:56:39 +0700

    create router and layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,15 @@ function App() {
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
-            const Layout =
-              route.layout === null ? React.Fragment : DefaultLayout;
             const Page = route.component;
+
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = React.Fragment;
+            }
+
             return (
               <Route
                 key={index}
