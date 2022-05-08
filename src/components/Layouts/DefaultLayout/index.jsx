9105bb35361e0e8a235c0a78a61fbced7f0e5307
Do Tien Dat <datblu2003@gmail.com>
2022-05-08 19:41:14 +0700

    Build header UI

--- src/components/Layouts/DefaultLayout/index.jsx
+++ src/components/Layouts/DefaultLayout/index.jsx
@@ -1,19 +1,17 @@
-import Header from "../components/Header";
-import SideBar from "./Sidebar";
-import classes from './DefaultLayout.module.scss'
-import classNames from 'classnames/bind'
+import Header from '../components/Header';
+import SideBar from './Sidebar';
+import classes from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
 
-const cx = classNames.bind(classes)
+const cx = classNames.bind(classes);
 
 const DefaultLayout = ({ children }) => {
   return (
     <div className={cx('wrapper')}>
       <Header />
       <div className={cx('container')}>
-        <SideBar /> 
-        <div className={cx('content')}>
-            {children}
-        </div>
+        <SideBar />
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
--- src/components/Layouts/components/Header/index.jsx
+++ src/components/Layouts/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
 import classes from './Header.module.scss';
 import images from '~/assets/images';
@@ -10,18 +10,16 @@ const Header = () => {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <div className={cx('logo')}>
-          <img src={images.logo} alt='tiktok'/>
-        </div>
+        <img src={images.logo} alt="tiktok" />
         <div className={cx('search')}>
-            <input placeholder='Searchs accounts and videos' spellCheck={false} />
-            <button className={cx('clear')}>
-                <FontAwesomeIcon icon={faCircleXmark} />
-            </button>
-            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-            <button className={cx('search-btn')}>
-                <FontAwesomeIcon icon={faSearch} />
-            </button>
+          <input placeholder="Searchs accounts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
         </div>
         <div className={cx('actions')} />
       </div>
